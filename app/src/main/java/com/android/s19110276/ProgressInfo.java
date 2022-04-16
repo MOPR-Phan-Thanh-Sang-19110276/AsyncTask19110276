fdbcce846ea1d8e353c897f9635e7351e5ed5d22
sangphan45 <sieungansang000@gmail.com>
2022-04-16 09:46:10 +0700

    commit

---
+++ app/src/main/java/com/android/s19110276/ProgressInfo.java
@@ -0,0 +1,20 @@
+package com.android.s19110276;
+
+public class ProgressInfo {
+
+    private int progress;
+    private String workingInfo;
+
+    public ProgressInfo(int progress, String workingInfo) {
+        this.progress = progress;
+        this.workingInfo = workingInfo;
+    }
+
+    public int getProgress() {
+        return progress;
+    }
+
+    public String getWorkingInfo() {
+        return workingInfo;
+    }
+}
